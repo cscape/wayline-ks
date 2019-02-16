74314543724e957da5e4b19a9c3ec7d52bb8a2bb
ciolt <cioltdev@outlook.com>
2019-02-16 12:43:37 -0500

    add env variable $PORT to server script for Heroku

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ async function start() {
 
   const {
     host = process.env.HOST || '127.0.0.1',
-    port = process.env.PORT || 3000
+    port = process.env.$PORT || process.env.PORT || 3000
   } = nuxt.options.server
 
   app.use('/feedproxy', async (req, res) => {
