1db9ae4bf4567d9e4d077cd0cc86827ec4d3b19b
ciolt <cioltdev@outlook.com>
2019-02-19 02:20:08 -0500

    new favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,14 +11,14 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Wayline Kitchen Sink',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/black-icon.png' }
     ]
   },
 
