b2024acd7c47cfdca8b164bb1c8ed438a0810964
ciolt <cioltdev@outlook.com>
2019-02-19 02:20:24 -0500

    update navigation bar

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,17 +1,23 @@
 <template lang="pug">
-  b-navbar.nav(toggleable='md', type='dark')
-    b-navbar-brand(href='/')
-      Logo
+  b-navbar.nav(toggleable='md', type='dark', :style='{ backgroundColor: dark === true ? "#121212" : "ff2222" }')
+    NuxtLink(to='/')
+      b-navbar-brand.mr-4
+        Logo
     b-navbar-toggle(target='nav_collapse')
     b-collapse#nav_collapse(is-nav='')
+      b-navbar-nav
+        b-nav-item(to='/welcome') Kitchen Sink
+        b-nav-item(to='/full') Full View
       b-navbar-nav.ml-auto
-        b-nav-item(href='https://github.com/cscape') Cyberscape
+        b-navbar-brand.mr-2(href='https://github.com/cscape')
+          img.d-block(height='18' src='https://cyberscape.co/splash.svg')
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
 
 export default {
+  props: ['dark'],
   components: {
     Logo
   }
@@ -21,5 +27,12 @@ export default {
 <style scoped>
 .nav {
   background-color: #ff2222;
+}
+.navbar-nav .nav-link {
+  color: rgba(255, 255, 255, 0.65);
+  font-size: 0.9em;
+  text-transform: uppercase;
+  letter-spacing: 0.02em;
+  font-weight: 500;
 }
 </style>
